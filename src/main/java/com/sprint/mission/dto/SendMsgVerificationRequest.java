191f212b3eeeea28e35767db6fe933f49e1c9b08
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-01-17 18:38:24 +0900

    feat(dto): SendMsgVerificationRequest
    - remove private constructor and of because not proper usage
    - change to record

--- src/main/java/com/sprint/mission/dto/SendMsgVerificationRequest.java
+++ src/main/java/com/sprint/mission/dto/SendMsgVerificationRequest.java
@@ -2,24 +2,5 @@ package com.sprint.mission.dto;
 
 import java.util.UUID;
 
-public class SendMsgVerificationRequest {
-    private final UUID userId;
-    private final UUID channelId;
-
-    private SendMsgVerificationRequest(UUID userId, UUID channelId) {
-        this.userId = userId;
-        this.channelId = channelId;
-    }
-
-    public static SendMsgVerificationRequest of(UUID userId, UUID channelId) {
-        return new SendMsgVerificationRequest(userId, channelId);
-    }
-
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public UUID getChannelId() {
-        return channelId;
-    }
+public record SendMsgVerificationRequest(UUID userId, UUID channelId) {
 }
